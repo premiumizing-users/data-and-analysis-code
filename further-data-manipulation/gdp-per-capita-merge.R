034647d9716b783f3f5bbd056cfb389071abb6ca
premiumizing-users <runge.j@northeastern.edu>
2024-06-27 00:37:46 +0200

    Update gdp-per-capita-merge.R

--- further-data-manipulation/gdp-per-capita-merge.R
+++ further-data-manipulation/gdp-per-capita-merge.R
@@ -25,7 +25,8 @@ colnames(data)[colnames(data)=="x"] <- "gdp_p_capita_2017"
 # impute mean for missing gdp
 data$gdp_p_capita_2017[is.na(data$gdp_p_capita_2017)==TRUE] <- mean(data$gdp_p_capita_2017, na.rm=TRUE)
 
-# create gdp per capita quintile variable
+### choose one of the following depending on the dataset
+# create gdp per capita quintile variable for data_3b
 quantile(data$gdp_p_capita_2017, probs = .2)
 quantile(data$gdp_p_capita_2017, probs = .4)
 quantile(data$gdp_p_capita_2017, probs = .6)
@@ -36,6 +37,18 @@ data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=10743.1 & data$gdp_p_capita_201
 data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=14224.85 & data$gdp_p_capita_2017<31952.98] <- 3
 data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=31952.98 & data$gdp_p_capita_2017<48223.16] <- 4
 data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=48223.16] <- 5
+
+# create gdp per capita quintile variable for data_2
+quantile(data$gdp_p_capita_2017, probs = .2)
+quantile(data$gdp_p_capita_2017, probs = .4)
+quantile(data$gdp_p_capita_2017, probs = .6)
+quantile(data$gdp_p_capita_2017, probs = .8)
+
+data$gdp_p_cap_quintiles <- 1
+data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=26647.5 & data$gdp_p_capita_2017<44469.91] <- 2
+data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=44469.91 & data$gdp_p_capita_2017<59531.66] <- 3
+data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=59531.66] <- 4
+#data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=59531.66] <- 5
 
 # delete actual gdp per capita
 data$gdp_p_capita_2017 <- NULL
