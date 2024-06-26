21fd138f4be667b2a29818ed50be381abc49c786
premiumizing-users <runge.j@northeastern.edu>
2024-06-27 00:30:07 +0200

    Update gdp-per-capita-merge.R

--- further-data-manipulation/gdp-per-capita-merge.R
+++ further-data-manipulation/gdp-per-capita-merge.R
@@ -1,3 +1,6 @@
+# define dataset for merge
+data <- data_3b # data_2
+
 # read in world development indicator gdp data
 gdp_data <- read.csv("API_NY.GDP.PCAP.CD_DS2_en_csv_v2_10515199.csv",
                      header=TRUE,
@@ -21,3 +24,19 @@ colnames(data)[colnames(data)=="x"] <- "gdp_p_capita_2017"
 
 # impute mean for missing gdp
 data$gdp_p_capita_2017[is.na(data$gdp_p_capita_2017)==TRUE] <- mean(data$gdp_p_capita_2017, na.rm=TRUE)
+
+# create gdp per capita quintile variable
+quantile(data$gdp_p_capita_2017, probs = .2)
+quantile(data$gdp_p_capita_2017, probs = .4)
+quantile(data$gdp_p_capita_2017, probs = .6)
+quantile(data$gdp_p_capita_2017, probs = .8)
+
+data$gdp_p_cap_quintiles <- 1
+data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=10743.1 & data$gdp_p_capita_2017<14224.85] <- 2
+data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=14224.85 & data$gdp_p_capita_2017<31952.98] <- 3
+data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=31952.98 & data$gdp_p_capita_2017<48223.16] <- 4
+data$gdp_p_cap_quintiles[data$gdp_p_capita_2017>=48223.16] <- 5
+
+# delete actual gdp per capita
+data$gdp_p_capita_2017 <- NULL
+
